d2bd988966eb7794356c1eda74acccfc8a79f8a6
Andrés Rodríguez <andres.rodriguez@lithersoft.com>
2015-05-07 22:17:50 -0300

    Update

--- kivymd/bottom_sheet.py
+++ kivymd/bottom_sheet.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from kivy.core.window import Window
 from kivy.metrics import dp
 from kivy.properties import StringProperty
 from kivy.uix.scrollview import ScrollView
@@ -16,6 +17,8 @@ class BottomSheet(MaterialBoxLayout):
 		self.sv = ScrollView(do_scroll_x=False)
 		self.list = List()
 		super(BottomSheet, self).__init__(**kwargs)
+		self.width = Window.width
+		Window.bind(on_width=self.setter("width"))
 		self.sv.add_widget(self.list)
 
 	def on_header(self, instance, value):
@@ -24,6 +27,12 @@ class BottomSheet(MaterialBoxLayout):
 			self.subheader.height = 0
 		else:
 			self.subheader.height = dp(56)
+
+	def add_widget(self, widget, index=0):
+		self.list.add_widget(widget, index)
+
+	def remove_widget(self, widget):
+		self.list.remove_widget(widget)
 
 	def open(self):
 		pass
